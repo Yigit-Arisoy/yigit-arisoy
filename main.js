d816fb8de0e8ccd87d03379e3163bb01eb303ca3
Yigit-Arisoy <arisoy.yigit@gmail.com>
2022-08-03 21:20:06 +0300

    3.08.22 Revamp
    + Changed the main section to a grid layout
    - Deleted contents of the about section which is to be added in a later update
    + Added the graduation counter

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
-window.addEventListener('scroll', reveal);
+window.addEventListener("scroll", reveal);
 
-    function reveal()
-    {
-      var reveals = document.querySelectorAll('.reveal');
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-      for(var i = 0; i < reveals.length; i++){
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
 
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
+    //Add or remove the class with transitions for opacity and position depending on the position of the element relative to the viewport
 
-        //Add or remove the class with transitions for opacity and position depending on the position of the element relative to the viewport
-
-        if(revealtop < windowheight - revealpoint){
-          reveals[i].classList.add('active');
-        }
-        else{
-          reveals[i].classList.remove('active');
-        }
-      }
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
+  }
+}
 
+let gradCountdown = () => {
+  // Set the date we're counting down to
+  let countDownDate = new Date("Oct 9, 2022 23:00:00").getTime();
 
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
 
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("countdown").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "Graduated";
+    }
+  }, 1000);
+};
+
+gradCountdown();
